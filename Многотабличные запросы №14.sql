96f5df2ec97e1758a6ad74c56e5edcac1c34e715
Legresa <79331242+Legresa@users.noreply.github.com>
2021-04-30 19:00:11 +0300

    Update Многотабличные запросы №14.sql

--- Многотабличные запросы №14.sql
+++ Многотабличные запросы №14.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW Student_5yearhobby AS
 SELECT st.*
 FROM student st, student_hobby sh
-WHERE st.id = sh.student_id AND sh.data_end IS NULL AND (to_char('2025-05-10'::date, 'YYYY')::int * 12 * 30 + to_char('2025-05-10'::date, 'MM')::int * 30 + to_char('2025-05-10'::date, 'DD')::int - to_char(sh.data_start, 'YYYY')::int * 12 * 30 + to_char(sh.data_start, 'MM')::int * 30 + to_char(sh.data_start, 'DD')::int) / 30 / 12 >= 5
+WHERE st.id = sh.student_id AND sh.data_end IS NULL AND (to_char('2025-06-12'::date, 'YYYY')::int * 12 * 30 + to_char('2025-06-12'::date, 'MM')::int * 30 + to_char('2025-06-12'::date, 'DD')::int - to_char(sh.data_start, 'YYYY')::int * 12 * 30 + to_char(sh.data_start, 'MM')::int * 30 + to_char(sh.data_start, 'DD')::int) / 30 / 12 >= 5
